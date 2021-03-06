93536a8841ad473e4efd74fafbda75e61b0b67fa
jorgeAP <jorgeagoiz@gmail.com>
2021-03-06 22:17:22 +0100

    Connect with API

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
+import axios from 'axios';
 import React from 'react';
 import SearchForm from './SearchForm';
+import Character from './Character';
 
 class App extends React.Component {
 
     onSearchSubmit = async (character) => {
-        console.log(character);
+        const name = character.replace(' ', '+');
+        const response = await axios.get(`https://www.breakingbadapi.com/api/characters?name=${name}`);
+        console.log(response);
     };
 
     render () {
---
+++ src/components/Character.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Character extends React.Component {
+
+    render () {
+        return (
+            <div>Character!!</div>
+        );
+    }
+};
+
+export default Character;
