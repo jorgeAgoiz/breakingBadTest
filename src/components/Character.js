08542a98454baed2498eec06db5c0da10e1c69a4
jorgeAP <jorgeagoiz@gmail.com>
2021-03-07 17:24:11 +0100

    BB Finish search engine

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,14 +3,25 @@ import React from 'react';
 
 const Character = (props) => {
 
-    return (
-        <div className="characterCard">
-            <h1>{props.charac.name}</h1>
-            <h3>{props.charac.nickname}</h3>
-            <p>{props.charac.status}</p>
-            <img src={props.charac.img} alt={props.charac.portrayed} />
-        </div>
-    );
+    let result;
+
+    if (!props.charac.name) {
+        result = (
+            <div className="loadTitle">
+                <h3>Search Your Character</h3>
+            </div>
+        );
+    } else {
+        result = (
+            <div className="characterCard">
+                <h1>{props.charac.name}</h1>
+                <h3>{props.charac.nickname}</h3>
+                <p>{props.charac.status}</p>
+                <img src={props.charac.img} alt={props.charac.portrayed} />
+            </div>
+        );
+    }
+    return result;
 };
 
 export default Character;
